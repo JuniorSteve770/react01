9bbedfcc3e58afbf41db382a9ad8d8b0422dca47
Your Name <juniorsteve770@gmail.com>
2020-11-07 22:39:40 +0100

    Updates

---
+++ precache-manifest.5b958363c867afda5c9b6d1edf709434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d798ee4be463b855ff18ba3a78f44faa",
+    "url": "/react01/index.html"
+  },
+  {
+    "revision": "79243c7489ad5475e7fd",
+    "url": "/react01/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "5dcd142ca0f8438bbcc0",
+    "url": "/react01/static/css/main.c33a8d85.chunk.css"
+  },
+  {
+    "revision": "79243c7489ad5475e7fd",
+    "url": "/react01/static/js/2.750e624b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react01/static/js/2.750e624b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcd142ca0f8438bbcc0",
+    "url": "/react01/static/js/main.409cbccf.chunk.js"
+  },
+  {
+    "revision": "efc50856df9f35694068",
+    "url": "/react01/static/js/runtime-main.4854a5bd.js"
+  }
+]);
